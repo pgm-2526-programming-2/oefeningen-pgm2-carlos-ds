8f44d032714e1a67234f0856fcac08ee39c5e7e0
Karel De Smet <karel.de.smet@outlook.com>
2025-11-21 10:08:08 +0100

    Add new exercises

---
+++ week-1/les-2/oef1.js
@@ -0,0 +1,18 @@
+// Schrijf een functie die het resultaat weergeeft van de som
+// van alle getallen van 1 tot en met n - ZONDER recursie
+function sumUp(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+// Schrijf een tweede functie die hetzelfde doet maar dan MET recursie
+function recursiveSumUp(n) {
+  if (n === 1) {
+    return 1;
+  }
+  
+  return n + recursiveSumUp(n - 1);
+}
---
+++ week-1/les-2/oef2.js
@@ -0,0 +1,5 @@
+// Schrijf een functie die het resultaat weergeeft van de vermeningvuldiging
+// van alle getallen van 1 tot en met n - ZONDER recursie
+
+
+// Schrijf een tweede functie die hetzelfde doet maar dan MET recursie
