0790180c9f9494989ffe5b5c63d93b42b21aaca7
Karel De Smet <karel.de.smet@outlook.com>
2025-11-21 10:55:48 +0100

    Solution for exercise 2

--- week-1/les-2/oef1.js
+++ week-1/les-2/oef1.js
@@ -13,6 +13,8 @@ function recursiveSumUp(n) {
   if (n === 1) {
     return 1;
   }
-  
+
   return n + recursiveSumUp(n - 1);
 }
+
+console.log(sumUp(100));
--- week-1/les-2/oef2.js
+++ week-1/les-2/oef2.js
@@ -2,7 +2,26 @@
 // van alle getallen van 1 tot en met n - ZONDER recursie
 // n = 3 -> 1 * 2 * 3 = 6
 // n = 4 -> 1 * 2 * 3 * 4 = 24
+function factorial(n) {
+  let total = 1;
+  for (let i = 1; i <= n; i++) {
+    total *= i;
+  }
+  return total;
+}
 
+console.log(factorial(3));
+console.log(factorial(4));
 
-// Schrijf een tweede functie recursiveFactorial(n) 
+// Schrijf een tweede functie recursiveFactorial(n)
 // die hetzelfde doet maar dan MET recursie
+function recursiveFactorial(n) {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n * recursiveFactorial(n - 1);
+  }
+}
+
+console.log(recursiveFactorial(3));
+console.log(recursiveFactorial(4));
