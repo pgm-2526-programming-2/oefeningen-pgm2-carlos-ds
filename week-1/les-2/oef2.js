6a1dbd6e55cf4b4b034e74bc594913acfcadf91f
Karel De Smet <karel.de.smet@outlook.com>
2025-11-21 10:12:51 +0100

    Edit description of second exercise

--- week-1/les-2/oef2.js
+++ week-1/les-2/oef2.js
@@ -1,5 +1,8 @@
-// Schrijf een functie die het resultaat weergeeft van de vermeningvuldiging
+// Schrijf een functie factorial(n) die het resultaat weergeeft van de vermeningvuldiging
 // van alle getallen van 1 tot en met n - ZONDER recursie
+// n = 3 -> 1 * 2 * 3 = 6
+// n = 4 -> 1 * 2 * 3 * 4 = 24
 
 
-// Schrijf een tweede functie die hetzelfde doet maar dan MET recursie
+// Schrijf een tweede functie recursiveFactorial(n) 
+// die hetzelfde doet maar dan MET recursie
