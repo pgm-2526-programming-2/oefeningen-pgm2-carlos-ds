d20d91c4a296466932e5e05aad355dc298085059
Karel De Smet <karel.de.smet@outlook.com>
2025-11-24 16:14:05 +0100

    Add new exercise

--- week-2/les-1/oef1.js
+++ week-2/les-1/oef1.js
@@ -1,5 +1,30 @@
 // Schrijf een functie die een array van gehele getallen als argument accepteert
 // en al die getallen bij elkaar optelt
 // 1. met een for loop
+function sumArray1(array) {
+  let sumArray = 0;
+  for (let i = 0; i < array.length; i++) {
+    sumArray += array[i];
+  }
+  return sumArray;
+}
+console.log(sumArray1([1, 2, 3]));
+
 // 2. met een for ... of loop
+function forOf(array) {
+  let totaal = 0;
+  for (const getal of array) {
+    totaal += getal;
+  }
+  return totaal;
+}
+
+console.log(forOf([1, 2, 3]));
+
 // 3. met forEach
+function forEachArray(array) {
+  let total = 0;
+  array.forEach((number) => (total += number));
+  return total;
+}
+console.log(forEachArray([1, 2, 3]));
---
+++ week-2/les-1/oef2.js
@@ -0,0 +1,8 @@
+/* Write a function accepts an array with temperatures in fahrenheit as an argument
+and returns an array with temperatures in celsius. 
+Look up for yourself how to convert from fahrenheit to celsius.
+Round the temperatures in celsius to 2 decimals. 
+e.g. [90, 80, 70] -> [32.22, 26.67, 21.11] */
+function mapTemperaturesToCelsius(temperatures) {
+  // Go!
+}
