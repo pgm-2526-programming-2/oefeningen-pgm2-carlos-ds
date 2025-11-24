91d987b9282dec628c53a82158b9378dd85f4207
Karel De Smet <karel.de.smet@outlook.com>
2025-11-24 14:05:39 +0100

    Add new exercise

---
+++ week-2/les-1/oef1.js
@@ -0,0 +1,5 @@
+// Schrijf een functie die een array van gehele getallen als argument accepteert
+// en al die getallen bij elkaar optelt
+// 1. met een for loop
+// 2. met een for ... of loop
+// 3. met forEach
